75dc43322f862bbd96d9997609a7f541bc6cb012
Roman Spazm <troyzemmann@gmail.com>
2015-10-21 18:00:15 +0300

    Начало верстки, настройка gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+var scss = require('gulp-scss');
+var watch = require('gulp-watch');
+var jade = require('gulp-jade');
+
+var paths = {
+	scss: 'app/scss/**/*.scss',
+	jade: 'app/**/*.jade'
+}
+ 
+gulp.task('serve', function() {
+  gulp.src('app')
+    .pipe(server({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('watch-scss', function() {
+	gulp.src(paths.scss)
+	.pipe(watch(paths.scss))
+	.pipe(scss({'bundleExec': false}))
+	.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch-jade', function() {
+	gulp.src(paths.jade)
+	.pipe(watch(paths.jade))
+	.pipe(jade({
+		pretty: true
+	}))
+	.pipe(gulp.dest('app/'));
+});
