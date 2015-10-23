ca89d15490824124eefe3d2dfe4396885e3af48b
Roman Spazm <troyzemmann@gmail.com>
2015-10-23 17:06:59 +0300

    COMPASS, настройка gulpfile, тест шаблонов JADE

---
+++ app/config.rb
@@ -0,0 +1,20 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/app"
+css_dir = "/app/css"
+sass_dir = "/app/scss"
+images_dir = "/app/img"
+javascripts_dir = "/app/scripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+output_style = :expanded
+
+relative_assets = false
+
+line_comments = true
+
+preferred_syntax = :scss
--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,20 @@ var server = require('gulp-server-livereload');
 var scss = require('gulp-scss');
 var watch = require('gulp-watch');
 var jade = require('gulp-jade');
+var compass = require('gulp-compass');
 
 var paths = {
 	scss: 'app/scss/**/*.scss',
-	jade: 'app/index.jade'
-}
+	jade: 'app/jade/pages/*.jade',
+	compass : {
+			configFile  : 'app/config.rb',
+			cssFolder   : 'app/css',
+			scssFolder  : 'app/scss',
+			imgFolder   : 'app/img'
+		}
+};
  
-gulp.task('serve', ['watch-scss', 'watch-jade'], function() {
+gulp.task('serve', ['compass', 'jade', 'watch'], function() {
   gulp.src('app')
     .pipe(server({
       livereload: true,
@@ -17,18 +24,33 @@ gulp.task('serve', ['watch-scss', 'watch-jade'], function() {
     }));
 });
 
-gulp.task('watch-scss', function() {
+gulp.task('scss', function() {
 	gulp.src(paths.scss)
 	.pipe(watch(paths.scss))
 	.pipe(scss({'bundleExec': false}))
 	.pipe(gulp.dest('app/css'));
 });
 
-gulp.task('watch-jade', function() {
+gulp.task('jade', function() {
 	gulp.src(paths.jade)
 	.pipe(watch(paths.jade))
 	.pipe(jade({
-		pretty: true
+		pretty: '\t'
 	}))
 	.pipe(gulp.dest('app/'));
+});
+
+gulp.task('compass', function() {
+	gulp.src(paths.scss)
+		.pipe(compass({
+			config_file: paths.compass.configFile,
+			css: paths.compass.cssFolder,
+			sass: paths.compass.scssFolder,
+			image: paths.compass.imgFolder
+		}));
+});
+
+gulp.task('watch', function(){
+	gulp.watch(paths.jade, ['jade']);
+	gulp.watch(paths.scss, ['compass']);
 });
