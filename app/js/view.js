5c9c2a11ad65f23d3467cc8f478a283c7cc320c8
Roman Spazm <troyzemmann@gmail.com>
2015-10-29 18:01:33 +0300

    Добавлен скрипт переключения отображения моделей

--- app/js/view.js
+++ app/js/view.js
@@ -1,8 +1,23 @@
 var viewModule = (function () {
 	var init = _view;
 
+	var viewLink = $('.filter__view-link');
+	var models = $('.model');
+
 	function _view() {
-		
+		viewLink.on('click', function(event) {
+			event.preventDefault();
+			var view = $(this).data('view');
+
+			models.each(function() {
+				$(this).attr('class', '');
+				$(this).addClass('model model_view-' + view);
+			});
+
+			console.log(view);
+		});
+
+		console.log('VIEW MODIFIER');
 	}
 
 	return {
